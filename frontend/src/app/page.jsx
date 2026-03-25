d7dc67e7a58cbbea7f2636c09e7b785db75b5262
Shashikant Giri <uselogin93@gmail.com>
2026-03-25 07:49:52 +0530

    add

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const { userId } = auth();
   if (userId) {
